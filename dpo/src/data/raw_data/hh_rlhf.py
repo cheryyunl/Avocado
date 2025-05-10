4fc0c042c57de17f82a546da31d583ea3e6444d4
cheryunl <cheryl@cheryunldeMacBook-Air.local>
2025-05-09 20:23:18 -0400

    update

--- dpo/src/data/raw_data/hh_rlhf.py
+++ dpo/src/data/raw_data/hh_rlhf.py
@@ -76,13 +76,15 @@ class MTHhRlhfRDP(RawDatasetPreprocessor):
     helpful_subdatasets: Optional[List[str]] = None
     
     def __post_init__(self):
-        self.harmless_subdatasets = ["harmless-base"]
+        if self.harmless_subdatasets is None:
+            self.harmless_subdatasets = ["harmless-base"]
         
-        self.helpful_subdatasets = [
-            "helpful-base",
-            "helpful-online",
-            "helpful-rejection-sampled",
-        ]
+        if self.helpful_subdatasets is None:
+            self.helpful_subdatasets = [
+                "helpful-base",
+                "helpful-online",
+                "helpful-rejection-sampled",
+            ]
 
     def _get_raw_dataset(self, split):
         # 加载所有数据集
@@ -213,17 +215,19 @@ class MTHhRlhfRDP(RawDatasetPreprocessor):
             "task_id":  example["task_id"]
         }
 
+@dataclass
 class TestHhRlhfRDP(RawDatasetPreprocessor):
     path: Optional[str] = "Anthropic/hh-rlhf"
     subdatasets: Optional[List[str]] = None
 
     def __post_init__(self):
-        self.subdatasets = [
-            "helpful-base", 
-            "helpful-online", 
-            "helpful-rejection-sampled",
-            "harmless-base"
-        ]
+        if self.subdatasets is None:
+            self.subdatasets = [
+                "helpful-base", 
+                "helpful-online", 
+                "helpful-rejection-sampled",
+                "harmless-base"
+            ]
 
     def _get_raw_dataset(self, split):
         all_datasets = []
